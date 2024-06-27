b6d9b5ac34e123ce9a85006b675d2272c1f52a49
hajaratali <hazarathalibibijan63@gmail.com>
2024-06-27 20:16:16 +0530

    new files

---
+++ SE&PM/p2.py
@@ -0,0 +1,45 @@
+# Program-8 program to show rotation, scaling, and translation on an image
+#Python program to explain cv2.rotate() method, cv2.resize(),translate
+
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+
+plt.figure(figsize=(20, 10))
+rows = 2
+columns = 2
+
+src = cv2.imread('th (1).jpg')
+src = src[:,:,::-1]
+
+plt.subplot(rows, columns, 1)
+plt.imshow(src)
+plt.axis('off')
+plt.title("Original")
+
+rot = cv2.rotate(src, cv2.ROTATE_90_CLOCKWISE)
+
+plt.subplot(rows, columns, 2)
+plt.imshow(rot)
+plt.axis('off')
+plt.title("Rotated")
+
+(h, w) = src.shape[:2]
+newdim = (100, h)
+img_resized = cv2.resize(src, newdim, interpolation=cv2.INTER_AREA)
+
+plt.subplot(rows, columns, 3)
+plt.imshow(img_resized)
+plt.axis('off')
+plt.title("Resized")
+
+M = np.float32([[1, 0, 25], [0, 1, 50]])
+shifted = cv2.warpAffine(src, M, (w, h))
+
+plt.subplot(rows, columns, 4)
+plt.imshow(shifted)
+plt.axis('off')
+plt.title("Translated")
+
+plt.tight_layout()
+plt.show()
---
+++ SE&PM/p5.py
@@ -0,0 +1,40 @@
+import cv2
+from matplotlib import pyplot as plt
+
+plt.figure(figsize=(14, 7))
+
+# Reading an image
+src = cv2.imread('th.jpg')
+
+# Convert image to grayscale
+gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+
+# Find Canny edges
+edged = cv2.Canny(gray, 30, 200)
+
+# Find contours
+contours, _ = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+# Display Canny edges
+plt.subplot(1, 2, 1)
+plt.imshow(edged, cmap='gray')
+plt.axis('off')
+plt.title("Canny Edges After Contouring")
+
+# Draw all contours on the original image
+cv2.drawContours(src, contours, -1, (0, 255, 0), 1)
+
+# Convert BGR image to RGB for displaying with matplotlib
+src_rgb = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
+
+# Display image with contours
+plt.subplot(1, 2, 2)
+plt.imshow(src_rgb)
+plt.axis('off')
+plt.title("Contours")
+
+plt.tight_layout()
+plt.show()
+
+# Print number of contours found
+print("Number of Contours found = " + str(len(contours)))
